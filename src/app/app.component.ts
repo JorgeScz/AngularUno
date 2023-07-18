b774256473dc274580c328e3834be8f6dc46ea64
JorgeScz <JorgeScz@users.noreply.github.com>
2023-07-17 20:30:41 -0600

    Version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './models/product.model';  
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'proyectoUno';
+  title = 'Proyecto Uno Angular';
+  http = inject(HttpClient);
+  products: Product[] = [];
+
+  changeTitle(){
+    this.title = 'Ejemplo: Uso de API'
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, // Módulo necesario para la ejecución en un navegador web
+    AppRoutingModule, // Módulo de enrutamiento para navegar entre páginas de la aplicación
+    HttpClientModule   // Módulo necesario para realizar solicitudes HTTP
   ],
   providers: [],
+  // Componente raíz de la aplicación
   bootstrap: [AppComponent]
 })
 export class AppModule { }
